60b240d18d7b7b263edec17db308b355b59ee698
gilberto solano <ossus@gilbertos-MacBook-Pro.local>
2021-09-11 20:51:17 -0600

    Adding new changes

--- cypress/integration/pageObjects/login.js
+++ cypress/integration/pageObjects/login.js
@@ -1,16 +1,23 @@
 class Login {
 
     email(){
-       return cy.get('input[type="email"]');
+       return cy.get('input[type="email"]')
     }
     password(){
-       return cy.get('input[type="password"]');
+       return cy.get('input[type="password"]')
     }
     textVisible(){
         return cy.get('input[type="text"]')
     }
     logInButton(){
         return cy.get('.auth0-label-submit').contains('Log In')
+    }
+    LoggedIn(){
+        cy.visit('https://app.labelbox.com/signin')
+        cy.get('input[type="email"]').type('gilberto-247@hotmail.com')
+        cy.get('input[type="password"]').type('Maxell1278.$')
+        cy.get('.auth0-label-submit').contains('Log In').click()
+        return this
     }
 }
 export default Login
--- cypress/integration/pageObjects/newProject.js
+++ cypress/integration/pageObjects/newProject.js
@@ -3,6 +3,19 @@ class Project {
     newProjectButton(){
        return cy.get('.jss18 > .MuiButtonBase-root > .MuiButton-label').contains('New project')
     }
+    projectInfoName(){
+        return cy.get('#name')
+    }
+    projectInfoDescription(){
+        return cy.get(':nth-child(2) > .MuiInputBase-root')
+    }
+    projectNextBtn(){
+        return cy.get('.MuiToolbar-root > .MuiButton-root > .MuiButton-label')
+    }
+    projectSearchInput(){
+        return cy.get('')
+    }
+
    
 }
 export default Project
