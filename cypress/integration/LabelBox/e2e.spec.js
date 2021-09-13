9608214f34f69499e1b61f3410e44a309bfb3e4c
gilberto solano <ossus@gilbertos-MacBook-Pro.local>
2021-09-13 10:47:57 -0600

    Adding new logic to page to page objects

--- cypress/integration/LabelBox/e2e.spec.js
+++ cypress/integration/LabelBox/e2e.spec.js
@@ -5,23 +5,21 @@ import Project from "../pageObjects/newProject";
 describe('End to End testing', () => {
     const login = new Login();
     const project = new Project();
-
+beforeEach(()=>{
+    login.LoggedIn()
+})
     it('User is able to log in successfully', () => {
-        cy.visit('https://app.labelbox.com/signin')
-        login.email().type('gilberto-247@hotmail.com') //remember to move this to env
-        login.password().type('Maxell1278.$')
-        login.logInButton().click()
     })
     it('User is able to create a new project',() => {
-        login.LoggedIn()
         project.newProjectButton().click()
         project.projectInfoName().type('Adding a new project')
         project.projectInfoDescription().type('Adding a description for my new project')
         project.projectNextBtn().click()
-        project.projectSearchInput().type('Example Nature Dataset{enter}')
-        //cy.contains('Attach').click()
-
+        project.projectAddData().click()
+        project.projectNextBtnStepTwo().click()
+        
     })
+
 
 
 
